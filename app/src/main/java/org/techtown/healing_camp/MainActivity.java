0261c5a2e0a736da0c83f58987797d5a9c926002
durnfish <117414891+durnfish@users.noreply.github.com>
2022-11-25 15:06:10 +0900

    11월 25일 모프수업

--- app/src/main/java/org/techtown/healing_camp/MainActivity.java
+++ app/src/main/java/org/techtown/healing_camp/MainActivity.java
@@ -3,12 +3,31 @@ package org.techtown.healing_camp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton buttonOnClickMakePlanner;
+    Button buttonOnClickWritePlaner;
+    LinearLayout layoutWritePlaner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonOnClickMakePlanner = (ImageButton) findViewById(R.id.buttonOnClickMakePlanner);
+        buttonOnClickWritePlaner = (Button) findViewById(R.id.buttonOnClickWritePlaner);
+        layoutWritePlaner = (LinearLayout)findViewById(R.id.layoutWritePlaner);
+
+        buttonOnClickMakePlanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              layoutWritePlaner.addView(buttonOnClickWritePlaner);
+            }
+        });
     }
 }
