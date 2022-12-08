171c7a5863c870fb896ccd4f0dc21234ff37eed5
durnfish <117414891+durnfish@users.noreply.github.com>
2022-12-08 20:20:55 +0900

    test

--- app/src/main/java/org/techtown/healing_camp/SearchCompleteActivity.java
+++ app/src/main/java/org/techtown/healing_camp/SearchCompleteActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchCompleteActivity extends AppCompatActivity {
     Button onClickBackLayer;
+    boolean flag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +22,22 @@ public class SearchCompleteActivity extends AppCompatActivity {
 
         onClickBackLayer = findViewById(R.id.onClickBackLayer);
 
+        ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == 1){
+                    setResult(1);
+                    finish();
+                };
+            }
+        });
         //테스트
         Button test = findViewById(R.id.test);
         test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setResult(1);
-                finish();
+                Intent intent = new Intent(SearchCompleteActivity.this, DetailViewActivity.class);
+                startActivityResult.launch(intent);
             }
         });
 
